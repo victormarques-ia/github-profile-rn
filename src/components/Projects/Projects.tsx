a44e3032dbd74e82fa7b40f764ad6270b68a67d4
Victor Silva <victormarques.ia@gmail.com>
2021-01-12 12:59:33 -0300

    feat: create Projects component

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as S from './Projects.elements';
+
+export interface ProjectProps {
+  name: string;
+  url: string;
+  language: string;
+}
+
+interface ProjectsProps {
+  projects: Array<ProjectProps>;
+}
+
+const Projects: React.FC<ProjectsProps> = ({ projects }: ProjectsProps) => (
+  <S.Container />
+);
+
+export default Projects;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ProfileHeader } from './ProfileHeader/ProfileHeader';
+export { default as Divider } from './Divider/Divider';
+export { default as Projects } from './Projects/Projects';
