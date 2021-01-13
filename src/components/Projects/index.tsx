8c4282eca3869fd6d1ae8e70b351c36993161ce0
Victor Silva <victormarques.ia@gmail.com>
2021-01-12 23:09:55 -0300

    refactor: update Projects component

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Project from './Project';
 
 import * as S from './styles';
 
 export interface ProjectProps {
   name: string;
-  url: string;
+  html_url: string;
   language: string;
 }
 
@@ -12,8 +13,27 @@ interface ProjectsProps {
   projects: Array<ProjectProps>;
 }
 
-const Projects: React.FC<ProjectsProps> = ({ projects }: ProjectsProps) => (
-  <S.Container />
-);
+const Projects: React.FC<ProjectsProps> = ({ projects }: ProjectsProps) => {
+  const renderProject = ({ index, item }) => (
+    <Project
+      index={index}
+      key={index}
+      name={item.name}
+      html_url={item.html_url}
+      language={item.language}
+    />
+  );
+
+  return (
+    <S.Container>
+      <S.List
+        data={projects}
+        renderItem={renderProject}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
+    </S.Container>
+  );
+};
 
 export default Projects;
