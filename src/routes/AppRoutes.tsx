efd862fd46ccdae197fad36b74d57b291db1a588
Victor Silva <victormarques.ia@gmail.com>
2021-01-13 00:55:42 -0300

    feat: create routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Profile } from '../views';
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <App.Navigator
+    screenOptions={{ headerShown: false }}
+    initialRouteName="Profile"
+  >
+    <App.Screen name="Home" component={Home} />
+    <App.Screen name="Profile" component={Profile} />
+  </App.Navigator>
+);
+
+export default AppRoutes;
